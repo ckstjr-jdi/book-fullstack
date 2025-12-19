34418998074695ede7de45b8123a824724838418
찬돌 <a50668151@gmail.com>
2025-12-19 16:58:15 +0900

    기초작업

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,11 +1,12 @@
-import React, { Fragment } from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from "react-dom/client";
+import App from "./App";
+//import BookApp from "./BookApp";
+import { BrowserRouter } from "react-router";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.querySelector("#root"));
 root.render(
-  <>
+  <BrowserRouter>
     <App />
-  </>
+  </BrowserRouter>
 );
