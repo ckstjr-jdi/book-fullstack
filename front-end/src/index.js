942ebabe0617483122badfda2194d7bde636f5f5
찬돌 <a50668151@gmail.com>
2025-12-19 17:06:50 +0900

    초기셋팅

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,8 +1,12 @@
 import ReactDOM from "react-dom/client";
-import App from "./App";
-//import BookApp from "./BookApp";
 import { BrowserRouter } from "react-router";
+import App from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css'
+import AuthLogic from "./service/authLogic";
+import app from "./service/firebase";
+// AuthLogic 객체 생성하기
+const authLogic = new AuthLogic(app);
+console.log(authLogic);
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 root.render(
